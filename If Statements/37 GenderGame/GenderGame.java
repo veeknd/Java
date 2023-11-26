188fb920759097dd3aa62979267ec49aa9a079c9
Veeknd <sethumanikkams@gmail.com>
2023-11-26 23:59:11 +0530

    gender game added

---
+++ If Statements/37 GenderGame/GenderGame.java
@@ -0,0 +1,29 @@
+from java.util import Scanner;
+
+class void GenderGAme{
+    class static void main(String[] args)
+    {
+        String gender;
+        String first_name;
+        String last_name;
+        int age;
+        String married;
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("What is your gender (M or F): ");
+        gender = keyboard.next();
+        System.out.print("First name: ");
+        first_name = keyboard.next();
+        System.out.print("Last name: ");
+        last_name = keyboard.next();
+        System.out.print("Age: ");
+        age = keyboard.nextInt();
+
+        System.out.print("Are you married: ");
+        married = keyboard.next();
+        if ()
+
+
+
+    }
+}
