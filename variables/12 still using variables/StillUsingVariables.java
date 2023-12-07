f102b78d9ec34b784d09fc786f2c015e9bfcd0b3
Veeknd <sethumanikkams@gmail.com>
2023-12-07 23:58:39 +0530

    12 added

---
+++ variables/12 still using variables/StillUsingVariables.java
@@ -0,0 +1,10 @@
+public class LetterToYourself
+{
+  public static void main( String[] args)
+  {
+{
+        String name = "Juan Vandez";
+        int year = 2010;
+        System.out.print("My name is " + name + " and I'll graduate in " + year);
+    }
+}
