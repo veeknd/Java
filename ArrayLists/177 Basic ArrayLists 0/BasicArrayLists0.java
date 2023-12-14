7052ad43c443f1a9d37cf1dd5f2f076a669fe15f
Veeknd <sethumanikkams@gmail.com>
2023-12-14 23:53:08 +0530

    added 177

---
+++ ArrayLists/177 Basic ArrayLists 0/BasicArrayLists0.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+public class BasicArrayLists0 {
+    public static void main (String[] args){
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        arr.add(-133);
+        arr.add(-133);
+        arr.add(-133);
+        arr.add(-133);
+        arr.add(-133);
+        arr.add(-133);
+        arr.add(-133);
+        arr.add(-133);
+        arr.add(-133);
+        arr.add(-133);
+
+        System.out.println("Slot 0 contains " + arr.get(0));
+        System.out.println("Slot 1 contains " + arr.get(1));
+        System.out.println("Slot 2 contains " + arr.get(2));
+        System.out.println("Slot 3 contains " + arr.get(3));
+        System.out.println("Slot 4 contains " + arr.get(4));
+        System.out.println("Slot 5 contains " + arr.get(5));
+        System.out.println("Slot 6 contains " + arr.get(6));
+        System.out.println("Slot 7 contains " + arr.get(7));
+        System.out.println("Slot 8 contains " + arr.get(8));
+        System.out.println("Slot 9 contains " + arr.get(9));
+
+    }
+}
